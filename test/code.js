a8e29e8d6d71260a2cd7fd013169722160244e19
xianzao <plwslpld@gmail.com>
2022-10-30 00:02:44 +0800

    🌟feat(test): init

---
+++ test/code.js
@@ -0,0 +1,23 @@
+const a = 12
+const b = 13
+debugger
+
+alert(123)
+
+// function debug() {
+//   console.log(123)
+// }
+
+const c = 15, // eslint-disable-line
+  debug = function () {
+    console.log(123)
+  }
+
+debug()
+
+function add(m, n) {
+  return m + n
+}
+add(a, b)
+
+console.log('end')
---
+++ test/index.js
@@ -0,0 +1,21 @@
+const fs = require('fs')
+const path = require('path')
+const babel = require('@babel/core')
+const noDebuggerPlugin = require('../index')
+
+const code = fs.readFileSync(path.resolve(__dirname, './code.js'), 'utf-8')
+
+const result = babel.transform(code, {
+  plugins: [
+    [
+      noDebuggerPlugin,
+      {
+        // console: true,
+        alert: true,
+        console: true
+      }
+    ]
+  ]
+})
+
+console.log(result.code)
